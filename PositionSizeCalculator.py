e9a82119b8eb3170135cda7e4cb4d980fdfe00ae
deep90s <deepinder90s@Gmail.com>
2023-08-01 00:32:56 +0530

    Update PositionSizeCalculator.py

--- PositionSizeCalculator.py
+++ PositionSizeCalculator.py
@@ -1,36 +1,28 @@
 import streamlit as st
-# The AdSense code snippet you copied
-adsense_code = """ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1846635093121225"
-     crossorigin="anonymous"></script> """
 
-# Embed the AdSense code using markdown with unsafe_allow_html
-st.markdown(adsense_code, unsafe_allow_html=True)
-def main():
-    st.title("Position Sizing Calculator")
+# Define a function to clear the input
+def clearInput(input_id):
+    """Clears the input with the given ID."""
+    st.session_state[input_id] = ''
 
-    # Clear the input box when clicked using JavaScript
-    st.markdown(
-        """
-        <script>
-        function clearInput(id) {
-            document.getElementById(id).value = "";
-        }
-        </script>
-        """,
-        unsafe_allow_html=True
-    )
+# Set default text for the inputs
+default_account_size = 100000
+default_max_stocks_portfolio = 10
+default_trade_entry = 100
+default_stoploss = 95
+default_risk_percentage = 2
 
-    # Get user inputs
-    account_size = st.number_input("Enter your account size:", step=0.01, key="account_size", on_click=clearInput)
-    max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1, key="max_stocks_portfolio", on_click=clearInput)
+# Get user inputs
+account_size = st.number_input("Enter your account size:", step=0.01, key="account_size", value=default_account_size, on_change=clearInput)
+max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1, key="max_stocks_portfolio", value=default_max_stocks_portfolio, on_change=clearInput)
 
-    # Get user inputs for a specific trade
-    trade_entry = st.number_input("Enter the trade entry point:", step=0.01, key="trade_entry", on_click=clearInput)
-    stoploss = st.number_input("Enter the stop loss price:", step=0.01, key="stoploss", on_click=clearInput)
-    risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01, key="risk_percentage", on_click=clearInput)
+# Get user inputs for a specific trade
+trade_entry = st.number_input("Enter the trade entry point:", step=0.01, key="trade_entry", value=default_trade_entry, on_change=clearInput)
+stoploss = st.number_input("Enter the stop loss price:", step=0.01, key="stoploss", value=default_stoploss, on_change=clearInput)
+risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01, key="risk_percentage", value=default_risk_percentage, on_change=clearInput)
 
-    # Rest of your code remains unchanged.
-    # ...
+# Rest of your code remains unchanged.
+# ...
 
 if __name__ == "__main__":
     main()
