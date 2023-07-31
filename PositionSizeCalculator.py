8d4f0b00554bba1581123896867c51f5e6cc3581
deep90s <deepinder90s@Gmail.com>
2023-08-01 00:42:34 +0530

    Update PositionSizeCalculator.py

--- PositionSizeCalculator.py
+++ PositionSizeCalculator.py
@@ -14,13 +14,13 @@ def main():
     default_risk_percentage = 2.0
 
     # Get user inputs
-    account_size = st.number_input("Enter your account size:", step=0.01, key="account_size", value=default_account_size, on_change=clearInput)
-    max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1, key="max_stocks_portfolio", value=default_max_stocks_portfolio, on_change=clearInput)
+    account_size = st.number_input("Enter your account size:", step=0.01, key="account_size", value=default_account_size, on_change=lambda: clearInput("account_size"))
+    max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1, key="max_stocks_portfolio", value=default_max_stocks_portfolio, on_change=lambda: clearInput("max_stocks_portfolio"))
 
     # Get user inputs for a specific trade
-    trade_entry = st.number_input("Enter the trade entry point:", step=0.01, key="trade_entry", value=default_trade_entry, on_change=clearInput)
-    stoploss = st.number_input("Enter the stop loss price:", step=0.01, key="stoploss", value=default_stoploss, on_change=clearInput)
-    risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01, key="risk_percentage", value=default_risk_percentage, on_change=clearInput)
+    trade_entry = st.number_input("Enter the trade entry point:", step=0.01, key="trade_entry", value=default_trade_entry, on_change=lambda: clearInput("trade_entry"))
+    stoploss = st.number_input("Enter the stop loss price:", step=0.01, key="stoploss", value=default_stoploss, on_change=lambda: clearInput("stoploss"))
+    risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01, key="risk_percentage", value=default_risk_percentage, on_change=lambda: clearInput("risk_percentage"))
 
     # Rest of your code remains unchanged.
     # ...
