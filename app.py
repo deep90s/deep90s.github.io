759521418e6edd7b1d361dbbfb54dfcbf93cb203
deep90s <deepinder90s@Gmail.com>
2023-07-31 21:57:19 +0530

    app.py
    Position Size Calculator for stocks

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
-# Get user inputs
-account_size = float(input("Enter your account size: "))
-max_stocks_portfolio = int(
-  input(
-    "Enter the maximum number of stocks you want to keep in your portfolio: "))
+import streamlit as st
 
-# Calculate capital per stock
-capital_per_stock = round(account_size / max_stocks_portfolio)
-print("Capital per stock:", capital_per_stock)
-# Get user inputs for a specific trade
-trade_entry = float(input("Enter the trade entry point: "))
-stoploss = float(input("Enter the stop loss price: "))
-risk_percentage = float(
-  input("Enter the percentage of capital you want to risk per trade: "))
+def main():
+    st.title("Position Sizing Calculator")
 
-# Calculate position size and round it to the nearest 1st digit
-stop_difference = trade_entry - stoploss
-position_size = round(
-  (account_size * (risk_percentage / 100)) / stop_difference, 1)
+    # Get user inputs
+    account_size = st.number_input("Enter your account size:", step=0.01)
+    max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1)
 
-# Calculate total buy value
-total_buy_value = round(position_size * trade_entry)
+    # ... Rest of the code ...
 
-# Calculate the percentage of the capital used for this trade
-percentage_used = round((total_buy_value / account_size) * 100, 2)
+    # Display the results
+    st.write("Account size:", account_size)
+    st.write("Maximum number of stocks in portfolio:", max_stocks_portfolio)
+    st.write("Trade entry point:", trade_entry)
+    st.write("Stop loss price:", stoploss)
+    st.write("Risk percentage per trade:", risk_percentage)
+    st.write("Position size per stock:", position_size, "Shares")
+    st.write("Total buy value:", total_buy_value)
+    st.write("Percentage of capital used for this trade:", percentage_used, "%")
 
-# Print the results
-print("\nAccount size:", account_size)
-print("Maximum number of stocks in portfolio:", max_stocks_portfolio)
-
-print("Trade entry point:", trade_entry)
-print("Stop loss price:", stoploss)
-print("Risk percentage per trade:", risk_percentage)
-print("Position size per stock:", position_size, "Shares")
-print("Total buy value:", total_buy_value)
-print("Percentage of capital used for this trade:", percentage_used, "%")
+if __name__ == "__main__":
+    main()
