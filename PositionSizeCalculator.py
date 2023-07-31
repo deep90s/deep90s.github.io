fb86aec0bd50a6381501ca20be49f2d5609ba54b
deep90s <deepinder90s@Gmail.com>
2023-08-01 00:14:52 +0530

    Update PositionSizeCalculator.py
    Clear default values of Text boxes

--- PositionSizeCalculator.py
+++ PositionSizeCalculator.py
@@ -1,55 +1,40 @@
 import streamlit as st
+from streamlit.components.html import html
+adsense_code = """
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1846635093121225"
+     crossorigin="anonymous"></script>
+"""
 
-# The AdSense code snippet you copied
-adsense_code = """<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1846635093121225"
-     crossorigin="anonymous"></script>"""
+# Embed the AdSense code using the HTML component
+st.components.html(adsense_code)
 
-# Embed the AdSense code using markdown with unsafe_allow_html
-st.markdown(adsense_code, unsafe_allow_html=True)
 
 def main():
     st.title("Position Sizing Calculator")
 
+    # Clear the input box when clicked using JavaScript
+    st.markdown(
+        """
+        <script>
+        function clearInput(id) {
+            document.getElementById(id).value = "";
+        }
+        </script>
+        """,
+        unsafe_allow_html=True
+    )
+
     # Get user inputs
-    account_size = st.number_input("Enter your account size:", step=0.01)
-    max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1)
+    account_size = st.number_input("Enter your account size:", step=0.01, key="account_size", on_change=clearInput)
+    max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1, key="max_stocks_portfolio", on_change=clearInput)
 
     # Get user inputs for a specific trade
-    trade_entry = st.number_input("Enter the trade entry point:", step=0.01)
-    stoploss = st.number_input("Enter the stop loss price:", step=0.01)
-    risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01)
+    trade_entry = st.number_input("Enter the trade entry point:", step=0.01, key="trade_entry", on_change=clearInput)
+    stoploss = st.number_input("Enter the stop loss price:", step=0.01, key="stoploss", on_change=clearInput)
+    risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01, key="risk_percentage", on_change=clearInput)
 
-    # Calculate position size and round it to the nearest 1st digit
-    stop_difference = trade_entry - stoploss
+    # Rest of your code remains unchanged.
+    # ...
 
-    if stop_difference == 0:
-        st.warning("Stop loss cannot be equal to trade entry. Please provide different inputs.")
-        return
-
-    position_size = round((account_size * (risk_percentage / 100)) / stop_difference, 1)
-
-    # Calculate total buy value
-    total_buy_value = round(position_size * trade_entry)
-
-    # Calculate the percentage of the capital used for this trade
-    percentage_used = round((total_buy_value / account_size) * 100, 2)
-
-    # Calculate how much money will be at risk per trade
-    money_at_risk_per_trade = round(position_size * stop_difference, 2)
-
-    # Display the results
-    st.write("Account size:", account_size)
-    st.write("Maximum number of stocks in portfolio:", max_stocks_portfolio)
-    st.write("Trade entry point:", trade_entry)
-    st.write("Stop loss price:", stoploss)
-    st.write("Risk percentage per trade:", risk_percentage)
-    st.write("Money at risk per trade:", money_at_risk_per_trade)
-    st.write("Position size per stock:", position_size, "Shares")
-    st.write("Total buy value:", total_buy_value)
-    st.write("Percentage of capital used for this trade:", percentage_used, "%")
-    
-
-    # Embed the AdSense code in the app
-    st.components.html(adsense_code)
 if __name__ == "__main__":
     main()
