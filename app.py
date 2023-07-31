de1fef45de92e3f098242fdbe6ca903457dd7eeb
deep90s <deepinder90s@Gmail.com>
2023-07-31 22:03:51 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ def main():
     account_size = st.number_input("Enter your account size:", step=0.01)
     max_stocks_portfolio = st.number_input("Enter the maximum number of stocks in your portfolio:", step=1)
 
-    # ... Rest of the code ...
+    # Get user inputs for a specific trade
+    trade_entry = st.number_input("Enter the trade entry point:", step=0.01)
+    stoploss = st.number_input("Enter the stop loss price:", step=0.01)
+    risk_percentage = st.number_input("Enter the percentage of capital you want to risk per trade:", step=0.01)
+
+    # Calculate position size and round it to the nearest 1st digit
+    stop_difference = trade_entry - stoploss
+    position_size = round((account_size * (risk_percentage / 100)) / stop_difference, 1)
+
+    # Calculate total buy value
+    total_buy_value = round(position_size * trade_entry)
+
+    # Calculate the percentage of the capital used for this trade
+    percentage_used = round((total_buy_value / account_size) * 100, 2)
 
     # Display the results
     st.write("Account size:", account_size)
